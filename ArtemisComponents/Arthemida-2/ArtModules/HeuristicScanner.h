45a8fde7d0dc651e8df31373517f12a185e2b169
NtKernelMC <50884710+NtKernelMC@users.noreply.github.com>
2021-04-20 20:27:03 +0300

    Исправлен трабл с итераторами эрэйсера С++20

--- ArtemisComponents/Arthemida-2/ArtModules/HeuristicScanner.h
+++ ArtemisComponents/Arthemida-2/ArtModules/HeuristicScanner.h
@@ -31,19 +31,19 @@ void __stdcall SigScanner(ArtemisConfig* cfg) // CAUTION! Not tested yet.
 		{
 			for (const auto& it : orderedMapping)
 			{
-				if (it.first == GetModuleHandleA("kernel32.dll")) continue;
+				if (it.first == (DWORD)GetModuleHandleA("kernel32.dll")) continue;
 				DWORD scanAddr = SigScan::FindPatternExplicit((DWORD)it.first, it.second,
 				std::get<0>(KeyValuePair.second), std::get<1>(KeyValuePair.second));
-				if (scanAddr != NULL && !Utils::IsVecContain(cfg->ExcludedSigAddresses, it.first))
+				if (scanAddr != NULL && !Utils::IsVecContain(cfg->ExcludedSigAddresses, (PVOID)it.first))
 				{
 					CHAR szFilePath[MAX_PATH + 1]; GetModuleFileNameA((HMODULE)it.first, szFilePath, MAX_PATH + 1);
 					std::string NameOfDLL = Utils::GetDllName(szFilePath);
 					MEMORY_BASIC_INFORMATION mme{ 0 }; ARTEMIS_DATA data; data.EmptyVersionInfo = true;
-					VirtualQuery(it.first, &mme, sizeof(MEMORY_BASIC_INFORMATION));
-					data.baseAddr = it.first; data.MemoryRights = mme.AllocationProtect;
+					VirtualQuery((PVOID)it.first, &mme, sizeof(MEMORY_BASIC_INFORMATION));
+					data.baseAddr = (PVOID)it.first; data.MemoryRights = mme.AllocationProtect;
 					data.regionSize = mme.RegionSize; data.dllName = NameOfDLL; data.dllPath = szFilePath;
 					data.HackName = KeyValuePair.first; data.type = DetectionType::ART_SIGNATURE_DETECT;
-					cfg->callback(&data); cfg->ExcludedSigAddresses.push_back(it.first);
+					cfg->callback(&data); cfg->ExcludedSigAddresses.push_back((PVOID)it.first);
 				}
 			}
 		}
