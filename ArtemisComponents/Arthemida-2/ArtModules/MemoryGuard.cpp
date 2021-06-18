b29525f9566f3cc252839ac6a97c4560022694ac
holmes0 <30002257+holmes000@users.noreply.github.com>
2021-06-18 13:44:09 +0400

    Fix description

--- ArtemisComponents/Arthemida-2/ArtModules/MemoryGuard.cpp
+++ ArtemisComponents/Arthemida-2/ArtModules/MemoryGuard.cpp
@@ -5,15 +5,6 @@
 */
 /**
 * Assignee: holmes0
-* Done: 
-*		Hook NtProtectVirtualMemory and block changes to executable memory
-*		Keep constantly updated list of memory page rights in order to detect any external modifications
-*			This still allows fast external protection changing and writing, impossible to perfectly protect from usermode.
-* 
-*		(FIXED) Bug: Page containing NtProtectVirtualMemory remains with PAGE_READWRITE_EXECUTE rights for hook library to remove and place hook back (consider switching to another library).
-*
-* TBD:
-*		WIP Enhancement: Multithreaded (faster) scanning to detect and prevent patching.
 */
 #include "MemoryGuard.h"
 using namespace ArtemisData;
